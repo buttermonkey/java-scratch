fa11db663a9c6cbac136a5101e4a1dcf665602f7
Markus Stoff <s52005@edu.campus02.at>
2023-02-25 14:20:09 +0100

    Add another Rectangle

--- src/java/pattern/Geometry.java
+++ src/java/pattern/Geometry.java
@@ -3,6 +3,7 @@ package pattern;
 public class Geometry {
 	public static void main(String[] args) {
 		printObject(new Rectangle(5, 7));
+		printObject(new Rectangle(4, 2));
 	}
 
 	private static void printObject(Object obj) {
